6c5534944a80d95efcb99f29958240cc0c86f7e6
Kang Minjun <97105309+joonamin@users.noreply.github.com>
2025-07-14 22:42:19 +0900

    [Bronze I] Title: Kinder Surprise, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ Java/백준/Bronze/23544. Kinder Surprise/Kinder Surprise.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            set.add(s);
+        }
+        System.out.println(n - set.size());
+    }
+
+}
