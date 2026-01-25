337172b440c4cdafa3ae9dbf3928bce455919191
Kang Minjun <97105309+joonamin@users.noreply.github.com>
2026-01-25 21:02:41 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2200 ms, Memory: 357516 KB -BaekjoonHub

--- Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,62 +1,47 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Map<Integer, Integer> syncMap;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         while (T-- > 0) {
-            syncMap = new HashMap<>();
-            PriorityQueue<Integer> minQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxQ = new PriorityQueue<>((a, b) -> -Integer.compare(a, b));
-            int Q = Integer.parseInt(br.readLine());
-            for (int i = 0; i < Q; i++) {
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = 0; i < k; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 char cmd = st.nextToken().charAt(0);
                 int value = Integer.parseInt(st.nextToken());
-                if (cmd == 'D') {
-                    PriorityQueue<Integer> tq = (value == 1) ? maxQ : minQ;
-                    pollQ(tq);
+
+                if (cmd == 'I') {
+                    map.put(value, map.getOrDefault(value, 0) + 1);
                 } else {
-                    syncMap.put(value, syncMap.getOrDefault(value, 0) + 1);
-                    minQ.add(value);
-                    maxQ.add(value);
+                    if (map.isEmpty())
+                        continue;
+
+                    int key;
+                    if (value == 1) {
+                        key = map.lastKey();
+                    } else {
+                        key = map.firstKey();
+                    }
+                    if (map.get(key) == 1) {
+                        map.remove(key);
+                    } else {
+                        map.put(key, map.get(key) - 1);
+                    }
                 }
             }
-            cleanQ(minQ);
-            cleanQ(maxQ);
-            Integer min = minQ.peek();
-            Integer max = maxQ.peek();
-            if ((min == null && max != null) || (min != null && max == null)) {
-                assert (false);
-            }
-            if (min == null && max == null) {
+
+            if (map.isEmpty()) {
                 sb.append("EMPTY\n");
             } else {
-                sb.append(String.format("%d %d\n", max, min));
+                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
             }
         }
         System.out.println(sb);
-    }
-
-    private static Integer pollQ(PriorityQueue<Integer> pq) {
-        cleanQ(pq);
-        if (!pq.isEmpty()) {
-            int value = pq.poll();
-            syncMap.put(value, syncMap.getOrDefault(value, 0) - 1);
-            return value;
-        }
-        return null;
-    }
-
-    private static void cleanQ(PriorityQueue<Integer> pq) {
-        while (!pq.isEmpty() && syncMap.get(pq.peek()) == 0) {
-            pq.poll();
-        }
     }
 }
